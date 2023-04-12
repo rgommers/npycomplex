455df1280c069b75ca02a7cee092086407780fac
Lysandros Nikolaou <lisandrosnik@gmail.com>
2023-04-12 13:10:50 +0200

    Add typedef according to env

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 #include <complex.h>
 #include <math.h>
 
-void print_complex(const char* note, double complex z)
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+typedef _Dcomplex dcomp;
+#else
+typedef double complex dcomp;
+#endif
+
+void print_complex(const char* note, dcomp z)
 {
     printf("%s %f%+f*i\n", note, creal(z), cimag(z));
 }
  
 int main(void)
 {
-    double complex z = -1.0 + 2.0*I;
+    dcomp z = -1.0 + 2.0*I;
     print_complex("z  =", z);
     print_complex("z\u00B2 =", z * z);
-    double complex z2 = ccos(2.0 * carg(z)) + csin(2.0 * carg(z))*I;
+    dcomp z2 = ccos(2.0 * carg(z)) + csin(2.0 * carg(z))*I;
     print_complex("z\u00B2 =", cabs(z) * cabs(z) * z2);
 }
