741ceca891a424741d73a13e0abd3f9461c156ec
Lysandros Nikolaou <lisandrosnik@gmail.com>
2023-04-12 14:11:55 +0200

    Test CBuild

--- main.c
+++ main.c
@@ -4,8 +4,12 @@
 
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 typedef _Dcomplex dcomp;
+#define CBuild(r, i) {r, i}
+#define CMul(r, i) _Cmulcc((r), (i))
 #else
 typedef double complex dcomp;
+#define CBuild(r, i) ((r) + (i) * I)
+#define CMul(r, i) ((r) * (i))
 #endif
 
 void print_complex(const char* note, dcomp z)
@@ -15,9 +19,9 @@ void print_complex(const char* note, dcomp z)
  
 int main(void)
 {
-    dcomp z = -1.0 + 2.0*I;
+    dcomp z = CBuild(-1.0, 2.0);
     print_complex("z  =", z);
-    print_complex("z\u00B2 =", z * z);
-    dcomp z2 = ccos(2.0 * carg(z)) + csin(2.0 * carg(z))*I;
-    print_complex("z\u00B2 =", cabs(z) * cabs(z) * z2);
+    print_complex("z\u00B2 =", CMul(z, z));
+    // dcomp z2 = ccos(2.0 * carg(z)) + csin(2.0 * carg(z))*I;
+    // print_complex("z\u00B2 =", cabs(z) * cabs(z) * z2);
 }
